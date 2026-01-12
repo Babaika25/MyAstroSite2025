80d8c0e93b998cab8a868e68aed93de576c51905
Babaika25 <valerylinkov2504@yandex.ru>
2026-01-13 00:19:54 +0300

    2026 update

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,7 +2,6 @@ export type ExperiencePosition = {
     title: string;
     year: string;
     description: string;
-    skills: string[];
 };
 
 export type Experience = {
@@ -14,7 +13,6 @@ export type EducationPosition = {
     title: string;
     year: string;
     description: string;
-    skills: string[];
 };
 
 export type Education = {
